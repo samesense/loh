29645c1ac1ab667b9e86bdcadfd94fe39115f1bd
Perry <samesense@gmail.com>
2010-08-31 11:05:20 -0400

    added new data and got NAs

--- plt.py
+++ plt.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import re
+import os
 
 def get_freqs(call, str_length, str):
     """Count minor allele freq by matching . or ,"""
@@ -13,27 +13,34 @@ def get_freqs(call, str_length, str):
     print call, str_length, str, freq
     return freq
 
-sample2alleles = defaultdict(dict)
-with open('data/perry.txt') as f:
-    for line in f:
-        sp = line.split('\t')
-        samples = sp[8].split('-')
-        idx = 17
-        for s in samples:
-            chr = sp[2].split('chr')[1]
-            pos = sp[3]
-            # keep if using at least 8x coverage
-            if int(sp[idx+2]) >= 8:
-                sample2alleles[s][chr + '.' + pos] = get_freqs(sp[idx],
-                                                               sp[idx+2],
-                                                               sp[idx+3])
-            idx += 5
-
-for s in sample2alleles:
-    ofile = 'working/chr_pos.' + s
-    with open(ofile, 'w') as o:
-        o.write('CHR\tMapInfo\tMinFreq\n')
-        for chr_pos in sample2alleles[s]:
-            chr, pos = chr_pos.split('.')
-            allele_freq = sample2alleles[s][chr_pos]
-            o.write(chr + '\t' + pos + '\t' + str(allele_freq) + '\n')
+data_dir = 'data/exome/'
+for afile in os.listdir(data_dir):
+    if not 'sun' in afile: # ignore exome.aa_chg.sun b/c is it redundant
+        file = os.path.join(data_dir, afile)
+        sample2alleles = defaultdict(dict)
+        with open(file) as f:
+            for line in f:
+                sp = line.split('\t')
+#                print str(sp)
+                samples = sp[8].split('-')
+                idx = 17
+                for s in samples:
+                    chr = sp[2].split('chr')[1]
+                    pos = sp[3]
+                    # keep if using at least 8x coverage
+                    if int(sp[idx+2]) >= 8:
+                        sample2alleles[s][chr + '.' + pos] = get_freqs(sp[idx],
+                                                                       sp[idx+2],
+                                                                       sp[idx+3])
+                    idx += 5
+        outdir = os.path.join('working',
+                              afile.replace('.', '_'))
+        os.system('mkdir -p ' + outdir)
+        for s in sample2alleles:
+            ofile = os.path.join(outdir, 'chr_pos.' + s)
+            with open(ofile, 'w') as o:
+                o.write('CHR\tMapInfo\tMinFreq\n')
+                for chr_pos in sample2alleles[s]:
+                    chr, pos = chr_pos.split('.')
+                    allele_freq = sample2alleles[s][chr_pos]
+                    o.write(chr + '\t' + pos + '\t' + str(allele_freq) + '\n')
--- r_plot.py
+++ r_plot.py
@@ -1,11 +1,12 @@
 import os, sys
 
-def do_file(filename):
-    new_file = filename.split('.')[-1] + '.png'
+def do_file(path, filename, plot_dir):
+    new_file = os.path.join(plot_dir,
+                            filename.split('.')[-1] + '.png')
     with open('rtmp', 'w') as f:
-        f.write("png('plots/" + new_file + "')\n")
+        f.write("png('" + new_file + "')\n")
         f.write('library(quantsmooth)\n')
-        f.write("MapInfo<-read.delim('working/" + filename + "',header=TRUE,sep='\t')\n")
+        f.write("MapInfo<-read.delim('" + path + "',header=TRUE,sep='\\t')\n")
         f.write("chrompos<-prepareGenomePlot(MapInfo[,1:2],paintCytobands=TRUE,organism='hsa')\n")
         f.write('for (i in 1:11) {segments(0,i+.5,3000000000,i+.5,col="blue",lwd=1)}\n')
         f.write("points(chrompos[,2],chrompos[,1]+MapInfo[,3],pch='.',col='red')\n")
@@ -13,8 +14,17 @@ def do_file(filename):
         f.write('dev.off()\n')
     os.system('R --vanilla < rtmp')
 
-for filename in os.listdir('working/'):
-    if 'chr_pos' in filename:
-        do_file(filename)
+working_dir = 'working'
+plot_dir = 'plots'
+for dir in os.listdir(working_dir):
+    if 'exome_aa_chg' in dir:
+        subdir = os.path.join(working_dir, dir)
+        new_plot_dir = os.path.join(plot_dir, dir)
+        os.system('mkdir -p ' + new_plot_dir)
+        for filename in os.listdir(subdir):
+            path = os.path.join(subdir,
+                                filename)
+            do_file(path, filename, new_plot_dir)
+        sys.exit(0)
 os.system('rm rtmp')
 
