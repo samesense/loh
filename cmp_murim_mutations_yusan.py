6326afc84f2e908d36518acf0bfb8dbafebd6a0b
Perry <samesense@gmail.com>
2010-09-03 20:27:46 -0400

    us/murim comparision looks good

--- cmp_murim_mutations_yusan.py
+++ cmp_murim_mutations_yusan.py
@@ -8,8 +8,21 @@ def get_mutations(normal, cancer):
 
     # chrpos
     mutants = {}
+    seen_chrpos = {}
     for chrpos in normal:
         if chrpos in cancer:
+            if normal[chrpos] != cancer[chrpos]:
+                mutants[chrpos] = normal[chrpos] + ':' + cancer[chrpos]
+            seen_chrpos[chrpos] = True
+    return (mutants, seen_chrpos)
+
+def get_mutations_limited(normal, cancer, limited):
+    """Given calls in normal and cancer, return the differences"""
+
+    # chrpos
+    mutants = {}
+    for chrpos in normal:
+        if chrpos in cancer and chrpos in limited:
             if normal[chrpos] != cancer[chrpos]:
                 mutants[chrpos] = normal[chrpos] + ':' + cancer[chrpos]
     return mutants
@@ -38,18 +51,33 @@ def load_murims_calls(afile, quality_cutoff):
     calls = {}
     with open(afile) as f:
         for line in f:
-            sp = line.split('\t')
-            if len(sp) == 29:
-                if '#' not in sp[0]:
+            have_data = False
+            if line[0] != '#':
+                sp = line.split('\t')
+                
+                # snpfilter
+#                 call = sp[5]
+#                 quality = sp[9]
+#                 chr = sp[2]
+#                 base = sp[3]
+#                have_data = True
+                
+                # shortall
+                if line[0:3] == 'chr':
                     call = sp[3]
                     quality = sp[6]
                     chr = sp[0]
                     base = sp[1]
-                    calls[chr+':'+base] = base
+                    have_data = True
+                if have_data:
+                    if int(quality) > quality_cutoff:
+                        calls[chr+':'+base] = call
     return calls
 
 quality_cut = 100
 murim_data_dir = 'data/murim/'
+#murim_suffix_normal = 'EX_BLD1_1ln.snpfilter_anno_v1.62.txt'
+#murim_suffix_cancer = 'EX_CANC_1ln.snpfilter_anno_v1.62.txt'
 murim_suffix_normal = 'EX_BLD1_1ln.snpfilter_anno_shortall_v1.62.txt'
 murim_suffix_cancer = 'EX_CANC_1ln.snpfilter_anno_shortall_v1.62.txt'
 our_data_dir = 'data/raw_hg18/'
@@ -61,9 +89,10 @@ us_normal = load_our_calls(os.path.join(our_data_dir,
 us_cancer = load_our_calls(os.path.join(our_data_dir,
                                         'yusanT', our_suffix),
                            quality_cut)
-us_mutants = get_mutations(us_normal, us_cancer)
+us_mutants, us_chrpos = get_mutations(us_normal, us_cancer)
+
 print 'US normal/cancer mutants'
-print len(us_normal), len(us_cancer), len(us_mutants)
+print str(len(us_normal)) + '/' + str(len(us_cancer)), len(us_mutants)
 
 murim_normal = load_murims_calls(os.path.join(murim_data_dir,
                                               'yusanN',
@@ -73,9 +102,15 @@ murim_cancer = load_murims_calls(os.path.join(murim_data_dir,
                                               'yusanT',
                                               murim_suffix_cancer), 
                                  quality_cut)
-murim_mutants = get_mutations(murim_normal, murim_cancer)
+murim_mutants, murim_chrpos = get_mutations(murim_normal, murim_cancer)
+
+murim_limited_mutants = get_mutations_limited(murim_normal, murim_cancer, us_chrpos)
+us_limited_mutants = get_mutations_limited(us_normal, us_cancer, murim_chrpos)
 
 print 'murim normal/cancer mutants'
-print len(murim_normal), len(murim_cancer), len(murim_mutants)
+print str(len(murim_normal)) + '/' +  str(len(murim_cancer)), len(murim_mutants)
+
+print 'us normal, murim normal differences', len(get_mutations(murim_normal, us_normal))
+print 'final mutation estimates us/murim', str(len(us_limited_mutants))+ '/'+ str(len(murim_limited_mutants))
 
 
